ac55bf1f803428f845aa49780245b094d2dd758b
DanMills1992 <DanMillsGit@outlook.com>
2022-10-30 11:37:19 +0000

    Add project files.

---
+++ ResidentLookup/ResidentLookupWindow.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace ResidentLookup
+{
+    partial class ResidentLookupWindow
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ResidenceDataGrid = new System.Windows.Forms.DataGridView();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.UserMessageLabel = new System.Windows.Forms.Label();
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.StreetNameTextbox = new System.Windows.Forms.TextBox();
+            this.StreetNumberTextbox = new System.Windows.Forms.TextBox();
+            this.StreetNameLabel = new System.Windows.Forms.Label();
+            this.StreetNumberLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ResidenceDataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ResidenceDataGrid
+            // 
+            this.ResidenceDataGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.ResidenceDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ResidenceDataGrid.Location = new System.Drawing.Point(29, 132);
+            this.ResidenceDataGrid.Name = "ResidenceDataGrid";
+            this.ResidenceDataGrid.ReadOnly = true;
+            this.ResidenceDataGrid.RowTemplate.Height = 25;
+            this.ResidenceDataGrid.Size = new System.Drawing.Size(565, 267);
+            this.ResidenceDataGrid.TabIndex = 3;
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(469, 98);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(125, 23);
+            this.SearchButton.TabIndex = 2;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // UserMessageLabel
+            // 
+            this.UserMessageLabel.Location = new System.Drawing.Point(-3, 54);
+            this.UserMessageLabel.Name = "UserMessageLabel";
+            this.UserMessageLabel.Size = new System.Drawing.Size(639, 24);
+            this.UserMessageLabel.TabIndex = 2;
+            this.UserMessageLabel.Text = "Please search by inputtingthe street name and number below.";
+            this.UserMessageLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TitleLabel.Location = new System.Drawing.Point(226, 9);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(173, 30);
+            this.TitleLabel.TabIndex = 3;
+            this.TitleLabel.Text = "Resident Lookup";
+            // 
+            // StreetNameTextbox
+            // 
+            this.StreetNameTextbox.AcceptsTab = true;
+            this.StreetNameTextbox.Location = new System.Drawing.Point(29, 99);
+            this.StreetNameTextbox.Name = "StreetNameTextbox";
+            this.StreetNameTextbox.Size = new System.Drawing.Size(150, 23);
+            this.StreetNameTextbox.TabIndex = 0;
+            // 
+            // StreetNumberTextbox
+            // 
+            this.StreetNumberTextbox.AcceptsTab = true;
+            this.StreetNumberTextbox.Location = new System.Drawing.Point(263, 99);
+            this.StreetNumberTextbox.Name = "StreetNumberTextbox";
+            this.StreetNumberTextbox.Size = new System.Drawing.Size(100, 23);
+            this.StreetNumberTextbox.TabIndex = 1;
+            this.StreetNumberTextbox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.StreetNumberTextbox_KeyPress);
+            // 
+            // StreetNameLabel
+            // 
+            this.StreetNameLabel.AutoSize = true;
+            this.StreetNameLabel.Location = new System.Drawing.Point(185, 102);
+            this.StreetNameLabel.Name = "StreetNameLabel";
+            this.StreetNameLabel.Size = new System.Drawing.Size(72, 15);
+            this.StreetNameLabel.TabIndex = 6;
+            this.StreetNameLabel.Text = "Street Name";
+            // 
+            // StreetNumberLabel
+            // 
+            this.StreetNumberLabel.AutoSize = true;
+            this.StreetNumberLabel.Location = new System.Drawing.Point(369, 103);
+            this.StreetNumberLabel.Name = "StreetNumberLabel";
+            this.StreetNumberLabel.Size = new System.Drawing.Size(51, 15);
+            this.StreetNumberLabel.TabIndex = 7;
+            this.StreetNumberLabel.Text = "Number";
+            // 
+            // ResidentLookupWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 430);
+            this.Controls.Add(this.StreetNumberLabel);
+            this.Controls.Add(this.StreetNameLabel);
+            this.Controls.Add(this.StreetNumberTextbox);
+            this.Controls.Add(this.StreetNameTextbox);
+            this.Controls.Add(this.TitleLabel);
+            this.Controls.Add(this.UserMessageLabel);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.ResidenceDataGrid);
+            this.Name = "ResidentLookupWindow";
+            this.Text = "Resident Lookup";
+            ((System.ComponentModel.ISupportInitialize)(this.ResidenceDataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView ResidenceDataGrid;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.Label UserMessageLabel;
+        private System.Windows.Forms.Label TitleLabel;
+        private System.Windows.Forms.TextBox StreetNameTextbox;
+        private System.Windows.Forms.TextBox StreetNumberTextbox;
+        private System.Windows.Forms.Label StreetNameLabel;
+        private System.Windows.Forms.Label StreetNumberLabel;
+    }
+}
+
---
+++ ResidentLookup/ResidentLookupWindow.cs
@@ -0,0 +1,142 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+using IronXL;
+using Newtonsoft.Json.Linq;
+
+namespace ResidentLookup
+{
+    public partial class ResidentLookupWindow : Form
+    {
+        DataTable Residents;
+        public ResidentLookupWindow()
+        {
+            string dataSource = ConfigurationManager.AppSettings["DataSource"];
+
+            InitializeComponent();
+
+            //Find the data file in the config and process it based on the type. If the datatype is not valid display message to the user.
+            if (dataSource.Substring(dataSource.LastIndexOf(".")) == ".xml")
+                Residents = GetXMLDataFile(ConfigurationManager.AppSettings["DataSource"]);
+            else if (dataSource.Substring(dataSource.LastIndexOf(".")) == ".csv")
+                Residents = GetCSVDataFile(ConfigurationManager.AppSettings["DataSource"]);
+            else
+                UserMessageLabel.Text = "Unable to load a data file. Please check config and make sure you are using either a .xml or .csv file.";
+        }
+
+        /// <summary>
+        /// Event to stop characters besides numbers entered into StreetNumberTextbox_
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StreetNumberTextbox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Checks whether the key pressed is not a number or control character(backspace) and ends the event if it isnt.
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        /// <summary>
+        /// The method used for filtering the dataset and displaying it on the screen
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SearchButton_Click(object sender, System.EventArgs e)
+        {
+            //If the data is not loaded do nothing.
+            if (Residents != null)
+            {
+                //Clear the previous data
+                ResidenceDataGrid.DataSource = null;
+
+                //Query on our stored data table with the users input.
+                var queryResults = Residents.AsEnumerable()
+                                            .Where(row => row.Field<string>("StreetName").ToLower().Contains(StreetNameTextbox.Text.ToLower())
+                                                        && row.Field<string>("HouseNumber") == StreetNumberTextbox.Text)
+                                            .OrderByDescending(row => row.Field<string>("Age"));
+
+                //Check whether the query results are null or 0 the either display the data and/or a message to the user.
+                if ((queryResults?.Count() ?? 0) != 0)
+                {
+                    UserMessageLabel.Text = "See your results below. Thank you for searching. ";
+                    ResidenceDataGrid.DataSource = queryResults.CopyToDataTable();
+                }
+                else
+                    UserMessageLabel.Text = "No results were returned. Please search again.";
+            }
+        }
+
+        /// <summary>
+        /// Method used to get data from the XML file. Only returns first DataTable.
+        /// </summary>
+        /// <param name="pathname"></param>
+        /// <returns></returns>
+        private DataTable GetXMLDataFile(string pathname)
+        {
+            //Initialise the dataset class used to parse the XML in to
+            DataSet dataSetFromXML = new DataSet();
+            //Initialise the dataTable class which will be returned 
+            DataTable returnDataTable = new DataTable();
+            //Initialise the DataRow to be used to later to add rows to the table.
+            DataRow convertedRow;
+
+            //use the DataSet class's method to read the XML file
+            dataSetFromXML.ReadXml(pathname);
+
+            //Loop through all of the rows in the first table. Only accepts 1 table.
+            foreach(DataRow row in dataSetFromXML.Tables[0].Rows)
+            {
+                //convert the text in each record to be a JObect to make it easier to manipulate.
+                JObject convertedAddressRecordJSON = JObject.Parse("{" + row.ItemArray[0].ToString().Replace('=', ':').Replace("\"\r\n", "\",") + "}");
+
+                //Check if this is the first row being added and if it is loop through and add the columns to the dataTable
+                if (returnDataTable.Rows.Count == 0)
+                    foreach (var keyValue in convertedAddressRecordJSON)
+                        returnDataTable.Columns.Add(keyValue.Key);
+
+                //Initialise the DataRow to be added to the table.
+                convertedRow = returnDataTable.NewRow();
+                
+                //Loop through the JObject adding the values to the DataRow based on key value pairs
+                foreach (var keyValue in convertedAddressRecordJSON)
+                    convertedRow[keyValue.Key] = keyValue.Value;
+                
+                //Add the DataRow to the DataTable
+                returnDataTable.Rows.Add(convertedRow);
+            }
+            //Return the datatable.
+            return returnDataTable;
+        }
+
+        /// <summary>
+        /// Method used to get data from the CSV file. Only returns first worksheet.
+        /// </summary>
+        /// <param name="pathname"></param>
+        /// <returns></returns>
+        private DataTable GetCSVDataFile(string pathname)
+        {
+            //Load the CSV into a worbook using a method in the WorkBook class from the IronXL NuGet package.
+            WorkBook workbook = WorkBook.LoadCSV(pathname, fileFormat: ExcelFileFormat.XLSX, ",");
+            //Add the first worksheet as a DataTable using the method from the ToDataTable method from the WorkSheet class in IronXL.
+            DataTable residents = new DataTable();
+
+            foreach (var col in workbook.DefaultWorkSheet.Columns)
+            {
+                //Convert the column names to remove spaces to match the xml file.
+                col.Rows[0].StringValue = col.Rows[0].ToString().Replace(" ", string.Empty);
+                //Added columns in manuall to force them to be string values to avoid casting issues.
+                residents.Columns.Add(col.Rows[0].ToString().Replace(" ", string.Empty), typeof(string));
+            }
+
+            //Loads the workbook datatable into the already created residents datatable. Keeps already created columns
+            residents.Load(workbook.DefaultWorkSheet.ToDataTable(true).CreateDataReader(), LoadOption.OverwriteChanges);
+           
+            //Return the results
+            return residents;
+        }
+    }
+}
